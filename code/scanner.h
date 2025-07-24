65a0ff1fc340f99208cba722010a7e6418fd181d
Anna Ramirez <129480807+AnnaBananas2005@users.noreply.github.com>
2025-07-24 11:33:46 -0700

    Update scanner.h

--- code/scanner.h
+++ code/scanner.h
@@ -1,24 +1,25 @@
 #ifndef SCANNER_H
 #define SCANNER_H
-
 #include <iostream>
 #include <string>
 
+#include <winsock2.h>
+#include <ws2tcpip.h>
 
 class Scanner {
 	public:
 		const char* ip;
 		SOCKET sockConnect, ListenSocket, AcceptSocket;
 
-		void Scanner::socketCreation(const char* ip, int port);
-		void Scanner::bindSocket(const char* ip, int port);
-		void Scanner::listenForConnections(const char* ip, int port);
-		void Scanner::connectToServer(const char* ip, int port);
-		void Scanner::acceptConnections(const char* ip, int port);
-		void Scanner::processData(const char* ip, int port);
+		void socketCreation(const char* ip, int port);
+		void bindSocket(const char* ip, int port);
+		void listenForConnections(const char* ip, int port);
+		void connectToServer(const char* ip, int port);
+		void acceptConnections(const char* ip, int port);
+		void processData(const char* ip, int port);
 
 	private:
-		//will put smt here 
+		//deciding what to put
 };
 
 
