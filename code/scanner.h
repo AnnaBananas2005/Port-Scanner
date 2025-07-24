57d3d5419e73d98f49c391ac00b8238269b5d8b3
Anna Ramirez <129480807+AnnaBananas2005@users.noreply.github.com>
2025-07-24 15:57:53 -0700

    Update scanner.h

--- code/scanner.h
+++ code/scanner.h
@@ -9,16 +9,12 @@
 class Scanner {
 	public:
 		const char* ip;
-		SOCKET sockConnect, ListenSocket, AcceptSocket;
+		SOCKET sockConnect;
 		WSADATA wsaData;
 		sockaddr_in service;
 
 		void socketCreation(const char* ip, int port, bool& returnValue);
-		void bindSocket(const char* ip, int port, bool& returnValue);
-		void listenForConnections(const char* ip, int port, int& result, bool& returnValue);
 		void connectToServer(const char* ip, int port, bool& returnValue);
-		void acceptConnections(const char* ip, int port, bool& returnValue);
-		void processData(const char* ip, int port, bool& returnValue);
 };
 
 #endif
