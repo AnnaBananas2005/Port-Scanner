92c1f4a527e1c36c1f99ac376a6ebce1dde0a227
Anna Ramirez <129480807+AnnaBananas2005@users.noreply.github.com>
2025-07-24 15:53:32 -0700

    Update scannerForWindows.cpp

--- code/scannerForWindows.cpp
+++ code/scannerForWindows.cpp
@@ -9,57 +9,22 @@
 
 //Used: https://learn.microsoft.com/en-us/windows/win32/api/winsock2/
 
-const char* ip = "127.0.0.1";
-
 void Scanner::socketCreation(const char* ip, int port, bool& returnValue) {
 	int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
 	if (result != 0) { 
 		std::cout << "Cant find usable Winsock.dll (Dynamic Link Library)";
 		WSACleanup();
-		returnValue = false;
-	}
-	sockConnect = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); 
-}
-
-//Associates socket with local ip address & port
-void Scanner::bindSocket(const char* ip, int port, bool& returnValue){
-	int storeResult = 0;
-
-	ListenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);	
-	sockaddr_in service; 
-	service.sin_family = AF_INET;
-
-	inet_pton(AF_INET, ip, &service.sin_addr);	
-	service.sin_port = htons(port);
-
-	storeResult = bind(ListenSocket, (SOCKADDR*)&service, sizeof(service));
-	if (storeResult == SOCKET_ERROR) {
-		std::cout << "The bind has failed with an error.";
-		WSACleanup();
-		returnValue = false;
-	}
-}
-
-//Ready to accept connections
-void Scanner::listenForConnections(const char* ip, int port, int &result, bool& returnValue) {
-
-	if (listen(ListenSocket, SOMAXCONN) == SOCKET_ERROR) {
-		std::cout << "Listen socket failed with an error";
-		WSACleanup();
 		returnValue = false; 
 	}
-	std::cout << "Listening on socket..";
-	result = closesocket(ListenSocket);
-
-	if (result == SOCKET_ERROR) {
-		std::cout << "The listen socket result failed with an error.";
+	//socket - Creating  actual socket (TCP/UDP), using IPv4
+	sockConnect = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); 
+	if (sockConnect == INVALID_SOCKET) {
+		std::cout << "Socket creation failed with error" << std::endl;
 		WSACleanup();
 		returnValue = false;
+		return;
 	}
-	else {
-		std::cout << "Connection successful" << std::endl;
-		returnValue = true;	
-	}
+	returnValue = true;
 
 }
 
@@ -71,46 +36,33 @@ void Scanner::connectToServer(const char* ip, int port, bool& returnValue) {
 
 	int connectResult = connect(sockConnect, (SOCKADDR*)&service, sizeof(service));
 	if (connectResult == SOCKET_ERROR) {
-		std::cout << "The connecting result failed with an error.";
+		std::cout << "The connecting result failed with an error." << std::endl;
 		WSACleanup();
 		returnValue = false;
+		return; //signals to exit
 	}
 
-	std::cout << "Connecting to server..";
-	connectResult = closesocket(sockConnect);
-
-	if (connectResult == SOCKET_ERROR) {
-		std::cout << "The connect result failed with an error.";
-		WSACleanup();
-		returnValue = false;
-	}
+	std::cout << "Connected to server..";
+	returnValue = true;
+	
 }
 
-// Accepts connections from client
-void Scanner::acceptConnections(const char* ip, int port, bool& returnValue) {
-	AcceptSocket = accept(ListenSocket, NULL, NULL);
-	if (AcceptSocket == SOCKET_ERROR) {
-		std::cout << "The accept socket failed with an error.";
-		WSACleanup();
-		returnValue = false;
-	}
-	closesocket(ListenSocket);
-}
 
 //Data exchange via connected socket 
 void Scanner::processData(const char* ip, int port, bool& returnValue) {
 	int dataSending = 0;
 	int recvbuflen = DEFAULT_BUFLEN;
-
 	const char* sendbuf = "The client is sending data test";
 	char recvbuf[512]; //512 is the DEFAULT_BUFLEN
+
 
 	//This part is a handshake (confirming communication)
 	int sendFirstBuffer = send(sockConnect, sendbuf, (int)strlen(sendbuf), 0);
 	if (sendFirstBuffer == SOCKET_ERROR) {
-		std::cout << "The send failed with an error.";
+		std::cout << "The send failed with an error." << std::endl;
 		WSACleanup();
 		returnValue = false;
+		return;
 	}
 	do {
 		dataSending = recv(sockConnect, recvbuf, recvbuflen, 0);
@@ -119,13 +71,15 @@ void Scanner::processData(const char* ip, int port, bool& returnValue) {
 		}
 		else if (dataSending == 0) {
 			std::cout << "Connection closed";
+			break;
 		}
 		else {
 			std::cout << "Failed with error";
+			break;
 		}
 	} while (dataSending > 0);
 
 	closesocket(sockConnect);
 	WSACleanup();
+	returnValue = true;
 }
-
