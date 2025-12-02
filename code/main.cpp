13b33c16a5a6fe803c90c11303d9f1c407f4482b
Anna Ramirez <129480807+AnnaBananas2005@users.noreply.github.com>
2025-12-01 23:47:27 -0800

    Add port range scanning functionality

--- code/main.cpp
+++ code/main.cpp
@@ -3,20 +3,39 @@
 
 int main() {
 	int port = 0;
+	int startRange = 0; 
+	int endRange = 0;
 	const char* ip = "127.0.0.1";
 	bool returnValue = false;
 
-	std::cout << "Enter a port number: ";
-	std::cin >> port;
+	std::string choose = "";
+	std::cout << "Would you like to scan one port or a range? ('one' or 'range'): ";
+	std::cin >> choose;
 
 	Scanner scanner;
-	scanner.socketCreation(ip, port, returnValue);
-	if (returnValue) {
-		scanner.connectToServer(ip, port, returnValue);
+	if (choose == "one" || choose == "One") {
+		std::cout << "Enter a port number: ";
+		std::cin >> port;
+
+		scanner.socketCreation(ip, port, returnValue);
+		if (returnValue) {
+			scanner.connectToServer(ip, port, returnValue);
+		}
+
+		std::cout << "Port" << "\t" << "State" << std::endl;
+		std::cout << port << "\t" << (returnValue ? "OPEN" : "CLOSED") << std::endl;
+
 	}
 
-	std::cout << "Port" << "\t" << "State" << std::endl;
-	std::cout << port << "\t" << (returnValue ? "OPEN" : "CLOSED") << std::endl;
+	else if (choose == "range" || choose == "Range") {
+		std::cout << "Enter a range" << std::endl;
+		std::cout << "1: ";
+		std::cin >> startRange;
+		std::cout << "2: ";
+		std::cin >> endRange;
+
+		scanner.rangeScan(scanner, ip, startRange, endRange);
+	}
 
 	return 0;
 }
